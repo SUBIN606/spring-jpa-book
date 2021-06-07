4d2e1b8aec61358116bec44b5835ba2c92b77c9d
SUBIN LEE <subinlee@SUBINui-MacBookAir.local>
2021-06-07 10:47:48 +0900

    project setting

---
+++ src/main/java/book/example/board/domain/posts/Posts.java
@@ -0,0 +1,33 @@
+package book.example.board.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(Long id, String title, String content, String author) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
