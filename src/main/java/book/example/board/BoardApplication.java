f3f0f3a78e52f9af997b0a0345025c304678ad12
SUBIN LEE <subinlee@SUBINui-MacBookAir.local>
2021-06-10 15:56:25 +0900

    session db저장방식으로 변경

--- src/main/java/book/example/board/BoardApplication.java
+++ src/main/java/book/example/board/BoardApplication.java
@@ -3,6 +3,7 @@ package book.example.board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @EnableJpaAuditing	// JPA Auditing 활성화
 @SpringBootApplication
